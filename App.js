dde3781596ced7001a03de7e8074f1fe96e5790d
D-vanheesch <Davidvanheesch@gmail.com>
2021-02-26 19:04:42 +0100

    Made home page with API call for movies.

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler'
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import styles from "./styles";
 import { NavigationContainer } from '@react-navigation/native'; //Router (wrappen)
 import { createStackNavigator } from '@react-navigation/stack'; //Switch
-import HomeScreen from "./Home";
-
+import HomeIndex from "./screens/HomeIndex";
 const Stack = createStackNavigator() //switch (routes in)
 
-function Screen1( {navigation} ) {
-   return <View style={styles.container}>
-        <Text>NETFLIX</Text>
-        <StatusBar style="auto" />
-        <Button onPress={() => navigation.navigate('Home')} title={`Go to Homescreen`}>Home</Button>
-    </View>
-}
-
 export default function App() {
-    // console.log (styles)
   return (
       <NavigationContainer>
           <Stack.Navigator>
-              <Stack.Screen name="Start" component={Screen1} />
-              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Home" component={HomeIndex} />
           </Stack.Navigator>
       </NavigationContainer>
   );
---
+++ screens/HomeIndex.js
@@ -0,0 +1,82 @@
+import { Text, View, FlatList, StyleSheet, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function HomeIndex( {navigation} ) {
+
+    const [movies, setMovies] = useState([]);
+
+    // // const [search, setSearch] = useState('');
+
+    // // const [countries, setCountries] = useState();
+
+    const options = {
+        method: 'GET',
+        url: 'https://unogsng.p.rapidapi.com/search',
+        params: {
+            orderby: 'date',
+            limit: '100',
+            type: 'movie',
+            start_year: '1992',
+            end_year: '2019',
+        },
+        headers: {
+            'x-rapidapi-key': 'adc43e01efmsh4e33b30d1f57ef9p1c5965jsn4a48b5e075ce',
+            'x-rapidapi-host': 'unogsng.p.rapidapi.com'
+        }
+    };
+
+    useEffect(() => {
+        async function getMovies() {
+            try {
+                const response = await axios(options)
+
+                setMovies(response.data.results);
+
+                console.log ('DATA:', movies);
+
+            } catch (e) {
+                console.log (e);
+            }
+        }
+        getMovies();
+
+    }, [])
+
+    function goToMovie(id) {
+        navigation.navigation("MovieShow", ({id: id}));
+    }
+
+
+    return <View>
+        <Text>NETFLIX</Text>
+
+        <FlatList
+            data={movies}
+            renderItem={({ item }) => {
+                return <Pressable style={styles.listItem} onPress={() => goToMovie(item.synopsis)}>
+                    <Text>
+                    {item.title}
+                    ({item.imdbrating})
+                    ({item.synopsis})
+                </Text>
+                </Pressable>
+            }}
+            keyExtractor={item => item.id.toString()}
+
+            ItemSeparatorComponent={() => {
+                return <View style={styles.itemSeparator} />
+            }}
+        />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    itemSeparator: {
+        borderBottomColor: 'black',
+        borderWidth: 2
+    },
+    listItem: {
+
+    }
+})
